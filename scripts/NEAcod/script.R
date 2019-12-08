5ba5c99c17d322f81d518c7d2e35b06423755959
olavbr <olavbr@nr.no>
2019-12-08 12:10:38 +0100

    NEAcod script

--- scripts/NEAcod/script.R
+++ scripts/NEAcod/script.R
@@ -27,21 +27,9 @@ dat<-setup.sam.data(surveys=surveys,
 conf = defcon(dat)
 par<-defpar(dat,conf)
 fitStandard<-sam.fit(dat,conf,par)
-#save(fitStandard,file = "markdown/Herring/fitStandard.Rda")
 
 
-cn<-read.ices("data/NEAcod/cn.dat")
-cw<-read.ices("data/NEAcod/cw.dat")
-dw<-read.ices("data/NEAcod/dw.dat")
-lf<-read.ices("data/NEAcod/lf.dat")
-lw<-read.ices("data/NEAcod/lw.dat")
-mo<-read.ices("data/NEAcod/mo.dat")
-nm<-read.ices("data/NEAcod/nm.dat")
-pf<-read.ices("data/NEAcod/pf.dat")
-pm<-read.ices("data/NEAcod/pm.dat")
-sw<-read.ices("data/NEAcod/sw.dat")
-surveys<-read.ices("data/NEAcod/survey.dat")
-
+#Use external variance estimates
 varC = as.matrix(read.table("data/NEAcod/variances/varCatch.txt", sep = " "))
 attributes(cn)$weight = 1/varC
 varS1 = as.matrix(read.table("data/NEAcod/variances/varFLT15Cod.txt", sep = " "))
@@ -61,37 +49,32 @@ conf = defcon(dat)
 par<-defpar(dat,conf)
 fitWithVar<-sam.fit(dat,conf,par)
 
-
-
-#Validate
+#Validate assessment
 AIC(fitStandard, fitWithVar)
 
 resStandard = residuals(fitStandard) 
-resfitWithVar = residuals(fitWithVar)  
-plot(resStandard)
-plot(resWithVar)
-
+resWithVar = residuals(fitWithVar)  
 retroStandard = retro(fitStandard,year = 7)
 retroWithVar = retro(fitWithVar,year = 7)
+
 plot(retroStandard)
+mtext("Retro standard settings", line = 2,at = 1980, cex = 2.5)
 plot(retroWithVar)
+mtext("Retro external variances",  line = 2,at = 1980, cex = 2.5)
+
+plot(resStandard)
+mtext("OSA residuals standard settings", line = 2,at = 0, cex = 2.5)
+plot(resWithVar)
+mtext("OSA residuals external variances", line = 2,at = 0, cex = 2.5)
 
 
+#Find advised TAC
+F_sq = 697000
+F_tr = 0.6
+forecast(fit,catchval = c(F_sq,NA,NA,NA),fval = c(NA,F_tr,F_tr,F_tr))
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+#Fit SAM with currently used configurations
 cn<-read.ices("data/NEAcod/cn.dat")
 cw<-read.ices("data/NEAcod/cw.dat")
 dw<-read.ices("data/NEAcod/dw.dat")
@@ -119,23 +102,4 @@ conf<-loadConf(dat,"scripts/NEAcod/model.cfg", patch=TRUE)
 par<-defpar(dat,conf)
 fitCurrent<-sam.fit(dat,conf,par)
 
-
 AIC(fitStandard, fitWithVar,fitCurrent)
-
-resCurrent = residuals(fitCurrent) 
-retroCurrent = retro(fitCurrent,year = 7)
-
-plot(retroWithVar)
-
-
-
-
-
-
-F_sq = 697000
-F_tr = 0.6
-forecast(fit,catchval = c(F_sq,NA,NA,NA),fval = c(NA,F_tr,F_tr,F_tr))
-
-fitOnlyCatch = leaveout(fit, fleet = list(c(2,3,4,5)))
-
-
